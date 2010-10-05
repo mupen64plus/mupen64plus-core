e7fe9923d48a9e20e24ddf231e775fc2ad78ca66
Richard Goedeken <Richard@fascinationsoftware.com>
2010-10-04 21:44:47 -0400

    Updated cheat file to slightly newer format, keyed off of CRC instead of 'GoodName'

--- tools/cheat_code_convert.py
+++ tools/cheat_code_convert.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python
-#/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-# *   Mupen64plus - cheat_code_convert.sh                                   *
-# *   Mupen64Plus homepage: http://code.google.com/p/mupen64plus/           *
-# *   Copyright (C) 2010 (Spinout)                                          *
-# *                                                                         *
-# *   This program is free software; you can redistribute it and/or modify  *
-# *   it under the terms of the GNU General Public License as published by  *
-# *   the Free Software Foundation; either version 2 of the License, or     *
-# *   (at your option) any later version.                                   *
-# *                                                                         *
-# *   This program is distributed in the hope that it will be useful,       *
-# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-# *   GNU General Public License for more details.                          *
-# *                                                                         *
-# *   You should have received a copy of the GNU General Public License     *
-# *   along with this program; if not, write to the                         *
-# *   Free Software Foundation, Inc.,                                       *
-# *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
-# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+'''* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *   Mupen64plus - code_convert.c                                          *
+ *   Mupen64Plus homepage: http://code.google.com/p/mupen64plus/           *
+ *   Copyright (C) 2010 Rhett Osborne                                      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
-#python code_convert.py > /home/spinout/mupen64plus/1.99.3/source/mupen64plus-core/data/mupencheat.txt < /home/spinout/mupen64plus/1.99.3/source/mupen64plus-core/data/mupen64plus.cht
+Usage:
+
+python code_convert.py > ../data/mupencheat.txt < ../data/mupen64plus.cht
+
+'''
 
 from sys import stdin
 
@@ -30,6 +34,7 @@ class cheat:
         self.d=""
         self.c=[]
         self.v=0
+        self.hb='00'
     def add(self, l):
         if(self.n == ""):
             return
@@ -66,15 +71,15 @@ lines = stdin.read().split("\n")
 
 for line in lines:
     if len(line) < 2: continue
-    if(line[:2] == "//"):
+    elif(line[:2] == "//" and line != "//----" and line != "//---" ):
         l.append(line)
-    if len(line) < 4: continue
-    if(line[0] == '['):
-        continue;
-    if(line[:5] == "Name="):
+    elif len(line) < 4: continue
+    elif(line[0] == '[' and line[-1] == ']' and len(line) > 23):
         print_l()
-        l.append("//\n//----\ngn %s" % (line[5:]))
-    if(line[:5] == "Cheat"):
+        l.append("\ncrc %s" % line[1:-1])
+    elif(line[:5] == "Name="):
+        l.append("gn %s" % (line[5:]))
+    elif(line[:5] == "Cheat"):
         t = line[5:].split('=')[0]
         if (len(t)>1 and t[-2] == '_'):
             n = int(t[:-2])
@@ -87,9 +92,9 @@ for line in lines:
                     if(option[-1]==','): end =-1
                     else: end = None
                     if(option[2] == " "):
-                        cs[n].c[cs[n].v] += "00%s:\"%s\""%(option[:2],option[3:end])
+                        cs[n].c[cs[n].v] += "%s%s:\"%s\""%(cs[n].hb,option[:2],option[3:end].replace("\"", "\\\""))
                     else:
-                        cs[n].c[cs[n].v] += "%s:\"%s\""%(option[:4],option[5:end])
+                        cs[n].c[cs[n].v] += "%s:\"%s\""%(option[:4],option[5:end].replace("\"", "\\\""))
                     cs[n].c[cs[n].v]+=','
                 cs[n].c[cs[n].v] = cs[n].c[cs[n].v][:-1]
         else:
@@ -100,8 +105,14 @@ for line in lines:
             i=0
             for cheat in cs[n].c:
                 if(cheat[-1] == '?'):
+                    if(cheat[-2:] == '??' and cheat[-4:-2] != '??'):
+                        cs[n].hb = cheat[-4:-2]
+                    else:
+                        cs[n].hb = '00'
                     cs[n].c[i] = cheat[:9] + "???? ";
                     cs[n].v=i
                 i+=1
         if(n > cCount):
             cCount = n
+    elif(line != "//----" and line != "//---" ):
+        l.append("//%s" %line)
