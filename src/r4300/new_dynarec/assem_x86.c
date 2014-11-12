436a33d7fa680d6d38bff0d411f3fded771fe5fd
Gillou68310 <gilles_siberlin@hotmail.fr>
2014-11-12 13:24:47 +0100

    new_dynarec: Fixed BGTZ/BLEZ
    When testing 64bit mips registers, the low 32bit part should be tested
    in unsigned mode.

--- src/r4300/new_dynarec/assem_x86.c
+++ src/r4300/new_dynarec/assem_x86.c
@@ -1671,6 +1671,20 @@ static void emit_jc(int a)
   output_byte(0x82);
   output_w32(a-(int)out-4);
 }
+static void emit_jae(int a)
+{
+  assem_debug("jae %x",a);
+  output_byte(0x0f);
+  output_byte(0x83);
+  output_w32(a-(int)out-4);
+}
+static void emit_jb(int a)
+{
+  assem_debug("jb %x",a);
+  output_byte(0x0f);
+  output_byte(0x82);
+  output_w32(a-(int)out-4);
+}
 
 static void emit_pushimm(int imm)
 {
