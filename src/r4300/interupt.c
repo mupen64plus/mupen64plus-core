fce823dd0e856334cab7cbd811f5bf8fb3e72fe2
Bobby Smiles <bobby.smiles32@gmail.com>
2015-02-04 00:49:25 +0100

    Extract *_int_handlers.

--- src/r4300/interupt.c
+++ src/r4300/interupt.c
@@ -420,6 +420,240 @@ static void wrapped_exception_general(void)
 #endif
 }
 
+static void special_int_handler(void)
+{
+    if (g_cp0_regs[CP0_COUNT_REG] > 0x10000000)
+        return;
+
+    remove_interupt_event();
+    add_interupt_event_count(SPECIAL_INT, 0);
+}
+
+static void vi_int_handler(void)
+{
+    if(vi_counter < 60)
+    {
+        if (vi_counter == 0)
+            cheat_apply_cheats(ENTRY_BOOT);
+        vi_counter++;
+    }
+    else
+    {
+        cheat_apply_cheats(ENTRY_VI);
+    }
+    gfx.updateScreen();
+#ifdef WITH_LIRC
+    lircCheckInput();
+#endif
+    SDL_PumpEvents();
+
+    timed_sections_refresh();
+
+    // if paused, poll for input events
+    if(rompause)
+    {
+        osd_render();  // draw Paused message in case gfx.updateScreen didn't do it
+        VidExt_GL_SwapBuffers();
+        while(rompause)
+        {
+            SDL_Delay(10);
+            SDL_PumpEvents();
+#ifdef WITH_LIRC
+            lircCheckInput();
+#endif //WITH_LIRC
+        }
+    }
+
+    new_vi();
+    if (g_vi.regs[VI_V_SYNC_REG] == 0) g_vi.delay = 500000;
+    else g_vi.delay = ((g_vi.regs[VI_V_SYNC_REG] + 1)*1500);
+    next_vi += g_vi.delay;
+    if (g_vi.regs[VI_STATUS_REG]&0x40) vi_field=1-vi_field;
+    else vi_field=0;
+
+    remove_interupt_event();
+    add_interupt_event_count(VI_INT, next_vi);
+
+    g_r4300.mi.regs[MI_INTR_REG] |= MI_INTR_VI;
+    if (g_r4300.mi.regs[MI_INTR_REG] & g_r4300.mi.regs[MI_INTR_MASK_REG])
+        g_cp0_regs[CP0_CAUSE_REG] = (g_cp0_regs[CP0_CAUSE_REG] | 0x400) & 0xFFFFFF83;
+    else
+        return;
+    if ((g_cp0_regs[CP0_STATUS_REG] & 7) != 1) return;
+    if (!(g_cp0_regs[CP0_STATUS_REG] & g_cp0_regs[CP0_CAUSE_REG] & 0xFF00)) return;
+    wrapped_exception_general();
+}
+
+static void compare_int_handler(void)
+{
+    remove_interupt_event();
+    g_cp0_regs[CP0_COUNT_REG]+=count_per_op;
+    add_interupt_event_count(COMPARE_INT, g_cp0_regs[CP0_COMPARE_REG]);
+    g_cp0_regs[CP0_COUNT_REG]-=count_per_op;
+
+    g_cp0_regs[CP0_CAUSE_REG] = (g_cp0_regs[CP0_CAUSE_REG] | 0x8000) & 0xFFFFFF83;
+    if ((g_cp0_regs[CP0_STATUS_REG] & 7) != 1) return;
+    if (!(g_cp0_regs[CP0_STATUS_REG] & g_cp0_regs[CP0_CAUSE_REG] & 0xFF00)) return;
+    wrapped_exception_general();
+}
+
+static void check_int_handler(void)
+{
+    remove_interupt_event();
+    wrapped_exception_general();
+}
+
+static void si_int_handler(void)
+{
+#ifdef WITH_LIRC
+    lircCheckInput();
+#endif //WITH_LIRC
+    SDL_PumpEvents();
+    g_si.pif.ram[0x3f] = 0x0;
+    remove_interupt_event();
+    g_r4300.mi.regs[MI_INTR_REG] |= MI_INTR_SI;
+    g_si.regs[SI_STATUS_REG] |= 0x1000;
+    if (g_r4300.mi.regs[MI_INTR_REG] & g_r4300.mi.regs[MI_INTR_MASK_REG])
+        g_cp0_regs[CP0_CAUSE_REG] = (g_cp0_regs[CP0_CAUSE_REG] | 0x400) & 0xFFFFFF83;
+    else
+        return;
+    if ((g_cp0_regs[CP0_STATUS_REG] & 7) != 1) return;
+    if (!(g_cp0_regs[CP0_STATUS_REG] & g_cp0_regs[CP0_CAUSE_REG] & 0xFF00)) return;
+    wrapped_exception_general();
+}
+
+static void pi_int_handler(void)
+{
+    remove_interupt_event();
+    g_r4300.mi.regs[MI_INTR_REG] |= MI_INTR_PI;
+    g_pi.regs[PI_STATUS_REG] &= ~3;
+    if (g_r4300.mi.regs[MI_INTR_REG] & g_r4300.mi.regs[MI_INTR_MASK_REG])
+        g_cp0_regs[CP0_CAUSE_REG] = (g_cp0_regs[CP0_CAUSE_REG] | 0x400) & 0xFFFFFF83;
+    else
+        return;
+    if ((g_cp0_regs[CP0_STATUS_REG] & 7) != 1) return;
+    if (!(g_cp0_regs[CP0_STATUS_REG] & g_cp0_regs[CP0_CAUSE_REG] & 0xFF00)) return;
+    wrapped_exception_general();
+}
+
+static void ai_int_handler(void)
+{
+    if (g_ai.regs[AI_STATUS_REG] & 0x80000000) // full
+    {
+        unsigned int ai_event = get_event(AI_INT);
+        remove_interupt_event();
+        g_ai.regs[AI_STATUS_REG] &= ~0x80000000;
+        g_ai.fifo[0].delay = g_ai.fifo[1].delay;
+        g_ai.fifo[0].length = g_ai.fifo[1].length;
+        add_interupt_event_count(AI_INT, ai_event+g_ai.fifo[1].delay);
+
+        g_r4300.mi.regs[MI_INTR_REG] |= MI_INTR_AI;
+        if (g_r4300.mi.regs[MI_INTR_REG] & g_r4300.mi.regs[MI_INTR_MASK_REG])
+            g_cp0_regs[CP0_CAUSE_REG] = (g_cp0_regs[CP0_CAUSE_REG] | 0x400) & 0xFFFFFF83;
+        else
+            return;
+        if ((g_cp0_regs[CP0_STATUS_REG] & 7) != 1) return;
+        if (!(g_cp0_regs[CP0_STATUS_REG] & g_cp0_regs[CP0_CAUSE_REG] & 0xFF00)) return;
+    }
+    else
+    {
+        remove_interupt_event();
+        g_ai.regs[AI_STATUS_REG] &= ~0x40000000;
+
+        //-------
+        g_r4300.mi.regs[MI_INTR_REG] |= MI_INTR_AI;
+        if (g_r4300.mi.regs[MI_INTR_REG] & g_r4300.mi.regs[MI_INTR_MASK_REG])
+            g_cp0_regs[CP0_CAUSE_REG] = (g_cp0_regs[CP0_CAUSE_REG] | 0x400) & 0xFFFFFF83;
+        else
+            return;
+        if ((g_cp0_regs[CP0_STATUS_REG] & 7) != 1) return;
+        if (!(g_cp0_regs[CP0_STATUS_REG] & g_cp0_regs[CP0_CAUSE_REG] & 0xFF00)) return;
+    }
+    wrapped_exception_general();
+}
+
+static void sp_int_handler(void)
+{
+    remove_interupt_event();
+    g_sp.regs[SP_STATUS_REG] |= 0x203;
+    // g_sp.regs[SP_STATUS_REG] |= 0x303;
+
+    if (!(g_sp.regs[SP_STATUS_REG] & 0x40)) return; // !intr_on_break
+    g_r4300.mi.regs[MI_INTR_REG] |= MI_INTR_SP;
+    if (g_r4300.mi.regs[MI_INTR_REG] & g_r4300.mi.regs[MI_INTR_MASK_REG])
+        g_cp0_regs[CP0_CAUSE_REG] = (g_cp0_regs[CP0_CAUSE_REG] | 0x400) & 0xFFFFFF83;
+    else
+        return;
+    if ((g_cp0_regs[CP0_STATUS_REG] & 7) != 1) return;
+    if (!(g_cp0_regs[CP0_STATUS_REG] & g_cp0_regs[CP0_CAUSE_REG] & 0xFF00)) return;
+    wrapped_exception_general();
+}
+
+static void dp_int_handler(void)
+{
+    remove_interupt_event();
+    g_dp.dpc_regs[DPC_STATUS_REG] &= ~2;
+    g_dp.dpc_regs[DPC_STATUS_REG] |= 0x81;
+    g_r4300.mi.regs[MI_INTR_REG] |= MI_INTR_DP;
+    if (g_r4300.mi.regs[MI_INTR_REG] & g_r4300.mi.regs[MI_INTR_MASK_REG])
+        g_cp0_regs[CP0_CAUSE_REG] = (g_cp0_regs[CP0_CAUSE_REG] | 0x400) & 0xFFFFFF83;
+    else
+        return;
+    if ((g_cp0_regs[CP0_STATUS_REG] & 7) != 1) return;
+    if (!(g_cp0_regs[CP0_STATUS_REG] & g_cp0_regs[CP0_CAUSE_REG] & 0xFF00)) return;
+    wrapped_exception_general();
+}
+
+static void hw2_int_handler(void)
+{
+    // Hardware Interrupt 2 -- remove interrupt event from queue
+    remove_interupt_event();
+    // setup r4300 Status flags: reset TS, and SR, set IM2
+    g_cp0_regs[CP0_STATUS_REG] = (g_cp0_regs[CP0_STATUS_REG] & ~0x00380000) | 0x1000;
+    g_cp0_regs[CP0_CAUSE_REG] = (g_cp0_regs[CP0_CAUSE_REG] | 0x1000) & 0xFFFFFF83;
+    /* the exception_general() call below will jump to the interrupt vector (0x80000180) and setup the
+     * interpreter or dynarec
+     */
+    wrapped_exception_general();
+}
+
+static void nmi_int_handler(void)
+{
+    // Non Maskable Interrupt -- remove interrupt event from queue
+    remove_interupt_event();
+    // setup r4300 Status flags: reset TS and SR, set BEV, ERL, and SR
+    g_cp0_regs[CP0_STATUS_REG] = (g_cp0_regs[CP0_STATUS_REG] & ~0x00380000) | 0x00500004;
+    g_cp0_regs[CP0_CAUSE_REG]  = 0x00000000;
+    // simulate the soft reset code which would run from the PIF ROM
+    r4300_reset_soft();
+    // clear all interrupts, reset interrupt counters back to 0
+    g_cp0_regs[CP0_COUNT_REG] = 0;
+    vi_counter = 0;
+    init_interupt();
+    // clear the audio status register so that subsequent write_ai() calls will work properly
+    g_ai.regs[AI_STATUS_REG] = 0;
+    // set ErrorEPC with the last instruction address
+    g_cp0_regs[CP0_ERROREPC_REG] = PC->addr;
+    // reset the r4300 internal state
+    if (r4300emu != CORE_PURE_INTERPRETER)
+    {
+        // clear all the compiled instruction blocks and re-initialize
+        free_blocks();
+        init_blocks();
+    }
+    // adjust ErrorEPC if we were in a delay slot, and clear the delay_slot and dyna_interp flags
+    if(delay_slot==1 || delay_slot==3)
+    {
+        g_cp0_regs[CP0_ERROREPC_REG]-=4;
+    }
+    delay_slot = 0;
+    dyna_interp = 0;
+    // set next instruction address to reset vector
+    last_addr = 0xa4000040;
+    generic_jump_to(0xa4000040);
+}
+
+
 void gen_interupt(void)
 {
     if (stop == 1)
@@ -462,222 +696,47 @@ void gen_interupt(void)
     switch(q.first->data.type)
     {
         case SPECIAL_INT:
-            if (g_cp0_regs[CP0_COUNT_REG] > 0x10000000) return;
-            remove_interupt_event();
-            add_interupt_event_count(SPECIAL_INT, 0);
+            special_int_handler();
             break;
+
         case VI_INT:
-            if(vi_counter < 60)
-            {
-                if (vi_counter == 0)
-                    cheat_apply_cheats(ENTRY_BOOT);
-                vi_counter++;
-            }
-            else
-            {
-                cheat_apply_cheats(ENTRY_VI);
-            }
-            gfx.updateScreen();
-#ifdef WITH_LIRC
-            lircCheckInput();
-#endif
-            SDL_PumpEvents();
-
-            timed_sections_refresh();
-
-            // if paused, poll for input events
-            if(rompause)
-            {
-                osd_render();  // draw Paused message in case gfx.updateScreen didn't do it
-                VidExt_GL_SwapBuffers();
-                while(rompause)
-                {
-                    SDL_Delay(10);
-                    SDL_PumpEvents();
-#ifdef WITH_LIRC
-                    lircCheckInput();
-#endif //WITH_LIRC
-                }
-            }
-
-            new_vi();
-            if (g_vi.regs[VI_V_SYNC_REG] == 0) g_vi.delay = 500000;
-            else g_vi.delay = ((g_vi.regs[VI_V_SYNC_REG] + 1)*1500);
-            next_vi += g_vi.delay;
-            if (g_vi.regs[VI_STATUS_REG]&0x40) vi_field=1-vi_field;
-            else vi_field=0;
-
-            remove_interupt_event();
-            add_interupt_event_count(VI_INT, next_vi);
-    
-            g_r4300.mi.regs[MI_INTR_REG] |= MI_INTR_VI;
-            if (g_r4300.mi.regs[MI_INTR_REG] & g_r4300.mi.regs[MI_INTR_MASK_REG])
-                g_cp0_regs[CP0_CAUSE_REG] = (g_cp0_regs[CP0_CAUSE_REG] | 0x400) & 0xFFFFFF83;
-            else
-                return;
-            if ((g_cp0_regs[CP0_STATUS_REG] & 7) != 1) return;
-            if (!(g_cp0_regs[CP0_STATUS_REG] & g_cp0_regs[CP0_CAUSE_REG] & 0xFF00)) return;
-            wrapped_exception_general();
+            vi_int_handler();
             break;
     
         case COMPARE_INT:
-            remove_interupt_event();
-            g_cp0_regs[CP0_COUNT_REG]+=count_per_op;
-            add_interupt_event_count(COMPARE_INT, g_cp0_regs[CP0_COMPARE_REG]);
-            g_cp0_regs[CP0_COUNT_REG]-=count_per_op;
-    
-            g_cp0_regs[CP0_CAUSE_REG] = (g_cp0_regs[CP0_CAUSE_REG] | 0x8000) & 0xFFFFFF83;
-            if ((g_cp0_regs[CP0_STATUS_REG] & 7) != 1) return;
-            if (!(g_cp0_regs[CP0_STATUS_REG] & g_cp0_regs[CP0_CAUSE_REG] & 0xFF00)) return;
-            wrapped_exception_general();
+            compare_int_handler();
             break;
     
         case CHECK_INT:
-            remove_interupt_event();
-            wrapped_exception_general();
+            check_int_handler();
             break;
     
         case SI_INT:
-#ifdef WITH_LIRC
-            lircCheckInput();
-#endif //WITH_LIRC
-            SDL_PumpEvents();
-            g_si.pif.ram[0x3f] = 0x0;
-            remove_interupt_event();
-            g_r4300.mi.regs[MI_INTR_REG] |= MI_INTR_SI;
-            g_si.regs[SI_STATUS_REG] |= 0x1000;
-            if (g_r4300.mi.regs[MI_INTR_REG] & g_r4300.mi.regs[MI_INTR_MASK_REG])
-                g_cp0_regs[CP0_CAUSE_REG] = (g_cp0_regs[CP0_CAUSE_REG] | 0x400) & 0xFFFFFF83;
-            else
-                return;
-            if ((g_cp0_regs[CP0_STATUS_REG] & 7) != 1) return;
-            if (!(g_cp0_regs[CP0_STATUS_REG] & g_cp0_regs[CP0_CAUSE_REG] & 0xFF00)) return;
-            wrapped_exception_general();
+            si_int_handler();
             break;
     
         case PI_INT:
-            remove_interupt_event();
-            g_r4300.mi.regs[MI_INTR_REG] |= MI_INTR_PI;
-            g_pi.regs[PI_STATUS_REG] &= ~3;
-            if (g_r4300.mi.regs[MI_INTR_REG] & g_r4300.mi.regs[MI_INTR_MASK_REG])
-                g_cp0_regs[CP0_CAUSE_REG] = (g_cp0_regs[CP0_CAUSE_REG] | 0x400) & 0xFFFFFF83;
-            else
-                return;
-            if ((g_cp0_regs[CP0_STATUS_REG] & 7) != 1) return;
-            if (!(g_cp0_regs[CP0_STATUS_REG] & g_cp0_regs[CP0_CAUSE_REG] & 0xFF00)) return;
-            wrapped_exception_general();
+            pi_int_handler();
             break;
     
         case AI_INT:
-            if (g_ai.regs[AI_STATUS_REG] & 0x80000000) // full
-            {
-                unsigned int ai_event = get_event(AI_INT);
-                remove_interupt_event();
-                g_ai.regs[AI_STATUS_REG] &= ~0x80000000;
-                g_ai.fifo[0].delay = g_ai.fifo[1].delay;
-                g_ai.fifo[0].length = g_ai.fifo[1].length;
-                add_interupt_event_count(AI_INT, ai_event+g_ai.fifo[1].delay);
-         
-                g_r4300.mi.regs[MI_INTR_REG] |= MI_INTR_AI;
-                if (g_r4300.mi.regs[MI_INTR_REG] & g_r4300.mi.regs[MI_INTR_MASK_REG])
-                    g_cp0_regs[CP0_CAUSE_REG] = (g_cp0_regs[CP0_CAUSE_REG] | 0x400) & 0xFFFFFF83;
-                else
-                    return;
-                if ((g_cp0_regs[CP0_STATUS_REG] & 7) != 1) return;
-                if (!(g_cp0_regs[CP0_STATUS_REG] & g_cp0_regs[CP0_CAUSE_REG] & 0xFF00)) return;
-            }
-            else
-            {
-                remove_interupt_event();
-                g_ai.regs[AI_STATUS_REG] &= ~0x40000000;
-
-                //-------
-                g_r4300.mi.regs[MI_INTR_REG] |= MI_INTR_AI;
-                if (g_r4300.mi.regs[MI_INTR_REG] & g_r4300.mi.regs[MI_INTR_MASK_REG])
-                    g_cp0_regs[CP0_CAUSE_REG] = (g_cp0_regs[CP0_CAUSE_REG] | 0x400) & 0xFFFFFF83;
-                else
-                    return;
-                if ((g_cp0_regs[CP0_STATUS_REG] & 7) != 1) return;
-                if (!(g_cp0_regs[CP0_STATUS_REG] & g_cp0_regs[CP0_CAUSE_REG] & 0xFF00)) return;
-            }
-            wrapped_exception_general();
+            ai_int_handler();
             break;
 
         case SP_INT:
-            remove_interupt_event();
-            g_sp.regs[SP_STATUS_REG] |= 0x203;
-            // g_sp.regs[SP_STATUS_REG] |= 0x303;
-    
-            if (!(g_sp.regs[SP_STATUS_REG] & 0x40)) return; // !intr_on_break
-            g_r4300.mi.regs[MI_INTR_REG] |= MI_INTR_SP;
-            if (g_r4300.mi.regs[MI_INTR_REG] & g_r4300.mi.regs[MI_INTR_MASK_REG])
-                g_cp0_regs[CP0_CAUSE_REG] = (g_cp0_regs[CP0_CAUSE_REG] | 0x400) & 0xFFFFFF83;
-            else
-                return;
-            if ((g_cp0_regs[CP0_STATUS_REG] & 7) != 1) return;
-            if (!(g_cp0_regs[CP0_STATUS_REG] & g_cp0_regs[CP0_CAUSE_REG] & 0xFF00)) return;
-            wrapped_exception_general();
+            sp_int_handler();
             break;
     
         case DP_INT:
-            remove_interupt_event();
-            g_dp.dpc_regs[DPC_STATUS_REG] &= ~2;
-            g_dp.dpc_regs[DPC_STATUS_REG] |= 0x81;
-            g_r4300.mi.regs[MI_INTR_REG] |= MI_INTR_DP;
-            if (g_r4300.mi.regs[MI_INTR_REG] & g_r4300.mi.regs[MI_INTR_MASK_REG])
-                g_cp0_regs[CP0_CAUSE_REG] = (g_cp0_regs[CP0_CAUSE_REG] | 0x400) & 0xFFFFFF83;
-            else
-                return;
-            if ((g_cp0_regs[CP0_STATUS_REG] & 7) != 1) return;
-            if (!(g_cp0_regs[CP0_STATUS_REG] & g_cp0_regs[CP0_CAUSE_REG] & 0xFF00)) return;
-            wrapped_exception_general();
+            dp_int_handler();
             break;
 
         case HW2_INT:
-            // Hardware Interrupt 2 -- remove interrupt event from queue
-            remove_interupt_event();
-            // setup r4300 Status flags: reset TS, and SR, set IM2
-            g_cp0_regs[CP0_STATUS_REG] = (g_cp0_regs[CP0_STATUS_REG] & ~0x00380000) | 0x1000;
-            g_cp0_regs[CP0_CAUSE_REG] = (g_cp0_regs[CP0_CAUSE_REG] | 0x1000) & 0xFFFFFF83;
-            /* the exception_general() call below will jump to the interrupt vector (0x80000180) and setup the
-             * interpreter or dynarec
-             */
-            wrapped_exception_general();
+            hw2_int_handler();
             break;
 
         case NMI_INT:
-            // Non Maskable Interrupt -- remove interrupt event from queue
-            remove_interupt_event();
-            // setup r4300 Status flags: reset TS and SR, set BEV, ERL, and SR
-            g_cp0_regs[CP0_STATUS_REG] = (g_cp0_regs[CP0_STATUS_REG] & ~0x00380000) | 0x00500004;
-            g_cp0_regs[CP0_CAUSE_REG]  = 0x00000000;
-            // simulate the soft reset code which would run from the PIF ROM
-            r4300_reset_soft();
-            // clear all interrupts, reset interrupt counters back to 0
-            g_cp0_regs[CP0_COUNT_REG] = 0;
-            vi_counter = 0;
-            init_interupt();
-            // clear the audio status register so that subsequent write_ai() calls will work properly
-            g_ai.regs[AI_STATUS_REG] = 0;
-            // set ErrorEPC with the last instruction address
-            g_cp0_regs[CP0_ERROREPC_REG] = PC->addr;
-            // reset the r4300 internal state
-            if (r4300emu != CORE_PURE_INTERPRETER)
-            {
-                // clear all the compiled instruction blocks and re-initialize
-                free_blocks();
-                init_blocks();
-            }
-            // adjust ErrorEPC if we were in a delay slot, and clear the delay_slot and dyna_interp flags
-            if(delay_slot==1 || delay_slot==3)
-            {
-                g_cp0_regs[CP0_ERROREPC_REG]-=4;
-            }
-            delay_slot = 0;
-            dyna_interp = 0;
-            // set next instruction address to reset vector
-            last_addr = 0xa4000040;
-            generic_jump_to(0xa4000040);
+            nmi_int_handler();
             break;
 
         default:
