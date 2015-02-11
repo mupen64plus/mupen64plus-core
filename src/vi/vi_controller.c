mupen64plus/mupen64plus-core

commit f9308adc19bd63dc8527ebe1c4e468622b753908

Extract vi_vertical_interrupt_event and move it into vi_controller module.
- move related variables into vi_controller structure
- move gameshark vi_counter to main module.

src/vi/vi_controller.c
@@ -21,6 +21,7 @@
 
 #include "vi_controller.h"
 
+#include "main/main.h"
 #include "memory/memory.h"
 #include "plugin/plugin.h"
 #include "r4300/r4300_core.h"
@@ -38,6 +39,9 @@ void connect_vi(struct vi_controller* vi,
 void init_vi(struct vi_controller* vi)
 {
     memset(vi->regs, 0, VI_REGS_COUNT*sizeof(uint32_t));
+    vi->field = 0;
+
+    vi->delay = vi->next_vi = 5000;
 }
 
 
@@ -49,8 +53,8 @@ int read_vi_regs(void* opaque, uint32_t address, uint32_t* value)
     if (reg == VI_CURRENT_REG)
     {
         update_count();
-        vi->regs[VI_CURRENT_REG] = (vi->delay - (next_vi - g_cp0_regs[CP0_COUNT_REG]))/1500;
-        vi->regs[VI_CURRENT_REG] = (vi->regs[VI_CURRENT_REG] & (~1)) | vi_field;
+        vi->regs[VI_CURRENT_REG] = (vi->delay - (vi->next_vi - g_cp0_regs[CP0_COUNT_REG]))/1500;
+        vi->regs[VI_CURRENT_REG] = (vi->regs[VI_CURRENT_REG] & (~1)) | vi->field;
     }
 
     *value = vi->regs[reg];
@@ -91,5 +95,26 @@ int write_vi_regs(void* opaque, uint32_t address, uint32_t value, uint32_t mask)
     return 0;
 }
 
+void vi_vertical_interrupt_event(struct vi_controller* vi)
+{
+    gfx.updateScreen();
 
+    /* allow main module to do things on VI event */
+    new_vi();
+
+    /* update field (if in interlaced mode) */
+    vi->field = (vi->regs[VI_STATUS_REG] >> 6) & ~vi->field;
+
+    /* schedule next vertical interrupt */
+    vi->delay = (vi->regs[VI_V_SYNC_REG] == 0)
+            ? 500000
+            : (vi->regs[VI_V_SYNC_REG] + 1)*1500;
+
+    vi->next_vi += vi->delay;
+
+    add_interupt_event_count(VI_INT, vi->next_vi);
+
+    /* trigger interrupt */
+    raise_rcp_interrupt(vi->r4300, MI_INTR_VI);
+}
 

src/main/main.c
@@ -39,6 +39,7 @@
 #include "api/config.h"
 #include "api/m64p_config.h"
 #include "api/debugger.h"
+#include "api/m64p_vidext.h"
 #include "api/vidext.h"
 
 #include "main.h"
@@ -105,6 +106,8 @@ struct rdp_core g_dp;
 struct rsp_core g_sp;
 
 int g_delay_si = 0;
+
+int g_gs_vi_counter = 0;
 
 /** static (local) variables **/
 static int   l_CurrentFrame = 0;         // frame counter
@@ -729,7 +732,7 @@ void new_frame(void)
     }
 }
 
-void new_vi(void)
+static void apply_speed_limiter(void)
 {
     unsigned int CurrentFPSTime;
     static unsigned int LastFPSTime = 0;
@@ -770,7 +773,7 @@ void new_vi(void)
         if (IntegratedDelta < 0 && l_MainSpeedLimit)
         {
             TimeToWait = (IntegratedDelta > ThisFrameDelta) ? -IntegratedDelta : -ThisFrameDelta;
-            DebugMessage(M64MSG_VERBOSE, "    new_vi(): Waiting %ims", TimeToWait);
+            DebugMessage(M64MSG_VERBOSE, "    apply_speed_limiter(): Waiting %ims", TimeToWait);
             SDL_Delay(TimeToWait);
             // recalculate # of milliseconds that have passed since the last video interrupt,
             // taking into account the time we just waited
@@ -786,6 +789,50 @@ void new_vi(void)
     VIDeltasIndex = (VIDeltasIndex + 1) & 63;
 
     timed_section_end(TIMED_SECTION_IDLE);
+}
+
+/* TODO: make a GameShark module and move that there */
+static void gs_apply_cheats(void)
+{
+    if(g_gs_vi_counter < 60)
+    {
+        if (g_gs_vi_counter == 0)
+            cheat_apply_cheats(ENTRY_BOOT);
+        g_gs_vi_counter++;
+    }
+    else
+    {
+        cheat_apply_cheats(ENTRY_VI);
+    }
+}
+
+static void pause_loop(void)
+{
+    if(rompause)
+    {
+        osd_render();  // draw Paused message in case gfx.updateScreen didn't do it
+        VidExt_GL_SwapBuffers();
+        while(rompause)
+        {
+            SDL_Delay(10);
+            main_check_inputs();
+        }
+    }
+}
+
+/* called on vertical interrupt.
+ * Allow the core to perform various things */
+void new_vi(void)
+{
+    gs_apply_cheats();
+
+    main_check_inputs();
+
+    timed_sections_refresh();
+
+    pause_loop();
+
+    apply_speed_limiter();
 }
 
 static void connect_all(

src/main/main.h
@@ -61,6 +61,8 @@ extern m64p_frame_callback g_FrameCallback;
 
 extern int g_delay_si;
 
+extern int g_gs_vi_counter;
+
 const char* get_savestatepath(void);
 const char* get_savesrampath(void);
 

src/main/savestates.c
@@ -461,8 +461,8 @@ static int savestates_load_m64p(char *filepath)
 #endif
 
     next_interupt = GETDATA(curr, unsigned int);
-    next_vi = GETDATA(curr, unsigned int);
-    vi_field = GETDATA(curr, unsigned int);
+    g_vi.next_vi = GETDATA(curr, unsigned int);
+    g_vi.field = GETDATA(curr, unsigned int);
 
     // assert(savestateData+savestateSize == curr)
 
@@ -561,8 +561,8 @@ static int savestates_load_pj64(char *filepath, void *handle,
     next_interupt = (g_cp0_regs[CP0_COMPARE_REG] < vi_timer)
                   ? g_cp0_regs[CP0_COMPARE_REG]
                   : vi_timer;
-    next_vi = vi_timer;
-    vi_field = 0;
+    g_vi.next_vi = vi_timer;
+    g_vi.field = 0;
     *((unsigned int*)&buffer[0]) = VI_INT;
     *((unsigned int*)&buffer[4]) = vi_timer;
     *((unsigned int*)&buffer[8]) = COMPARE_INT;
@@ -1228,8 +1228,8 @@ static int savestates_save_m64p(char *filepath)
 #endif
 
     PUTDATA(curr, unsigned int, next_interupt);
-    PUTDATA(curr, unsigned int, next_vi);
-    PUTDATA(curr, unsigned int, vi_field);
+    PUTDATA(curr, unsigned int, g_vi.next_vi);
+    PUTDATA(curr, unsigned int, g_vi.field);
 
     to_little_endian_buffer(queue, 4, queuelength/4);
     PUTARRAY(queue, curr, char, queuelength);

src/r4300/interupt.c
@@ -52,10 +52,6 @@
 #include "reset.h"
 #include "new_dynarec/new_dynarec.h"
 
-unsigned int next_vi;
-int vi_field=0;
-static int vi_counter=0;
-
 int interupt_unsafe_state = 0;
 
 struct interrupt_event
@@ -351,12 +347,11 @@ void load_eventqueue_infos(char *buf)
 void init_interupt(void)
 {
     SPECIAL_done = 1;
-    next_vi = next_interupt = 5000;
-    g_vi.delay = next_vi;
-    vi_field = 0;
+
+    g_vi.delay = g_vi.next_vi = 5000;
 
     clear_queue();
-    add_interupt_event_count(VI_INT, next_vi);
+    add_interupt_event_count(VI_INT, g_vi.next_vi);
     add_interupt_event_count(SPECIAL_INT, 0);
 }
 
@@ -439,45 +434,8 @@ static void special_int_handler(void)
 
 static void vi_int_handler(void)
 {
-    if(vi_counter < 60)
-    {
-        if (vi_counter == 0)
-            cheat_apply_cheats(ENTRY_BOOT);
-        vi_counter++;
-    }
-    else
-    {
-        cheat_apply_cheats(ENTRY_VI);
-    }
-    gfx.updateScreen();
-
-    main_check_inputs();
-
-    timed_sections_refresh();
-
-    // if paused, poll for input events
-    if(rompause)
-    {
-        osd_render();  // draw Paused message in case gfx.updateScreen didn't do it
-        VidExt_GL_SwapBuffers();
-        while(rompause)
-        {
-            SDL_Delay(10);
-            main_check_inputs();
-        }
-    }
-
-    new_vi();
-    if (g_vi.regs[VI_V_SYNC_REG] == 0) g_vi.delay = 500000;
-    else g_vi.delay = ((g_vi.regs[VI_V_SYNC_REG] + 1)*1500);
-    next_vi += g_vi.delay;
-    if (g_vi.regs[VI_STATUS_REG]&0x40) vi_field=1-vi_field;
-    else vi_field=0;
-
     remove_interupt_event();
-    add_interupt_event_count(VI_INT, next_vi);
-
-    raise_rcp_interrupt(&g_r4300, MI_INTR_VI);
+    vi_vertical_interrupt_event(&g_vi);
 }
 
 static void compare_int_handler(void)
@@ -559,7 +517,7 @@ static void nmi_int_handler(void)
     r4300_reset_soft();
     // clear all interrupts, reset interrupt counters back to 0
     g_cp0_regs[CP0_COUNT_REG] = 0;
-    vi_counter = 0;
+    g_gs_vi_counter = 0;
     init_interupt();
     // clear the audio status register so that subsequent write_ai() calls will work properly
     g_ai.regs[AI_STATUS_REG] = 0;
@@ -589,7 +547,7 @@ void gen_interupt(void)
 {
     if (stop == 1)
     {
-        vi_counter = 0; // debug
+        g_gs_vi_counter = 0; // debug
         dyna_stop();
     }
 

src/r4300/interupt.h
@@ -26,9 +26,6 @@
 
 void init_interupt(void);
 
-extern int vi_field;
-extern unsigned int next_vi;
-
 // set to avoid savestates/reset if state may be inconsistent
 // (e.g. in the middle of an instruction)
 extern int interupt_unsafe_state;

src/vi/vi_controller.h
@@ -48,7 +48,10 @@ enum vi_registers
 struct vi_controller
 {
     uint32_t regs[VI_REGS_COUNT];
+    unsigned int field;
+
     unsigned int delay;
+    unsigned int next_vi;
 
     struct r4300_core* r4300;
 };
@@ -65,5 +68,7 @@ void init_vi(struct vi_controller* vi);
 
 int read_vi_regs(void* opaque, uint32_t address, uint32_t* value);
 int write_vi_regs(void* opaque, uint32_t address, uint32_t value, uint32_t mask);
+
+void vi_vertical_interrupt_event(struct vi_controller* vi);
 
 #endif

commit 7342554963c7cf2fd6554eaec1f52f5d692c0990

Fix vi->field update when in interlaced mode.
vi->field value is assumed to be either 0 or 1 for VI_CURRENT_REG
update. The previous vi->field update formula did not guarantee that,
and therefore could lead to corrupted VI_CURRENT_REG values.

This regression was introduced in commit f9308adc.

src/vi/vi_controller.c
@@ -102,8 +102,8 @@ void vi_vertical_interrupt_event(struct vi_controller* vi)
     /* allow main module to do things on VI event */
     new_vi();
 
-    /* update field (if in interlaced mode) */
-    vi->field = (vi->regs[VI_STATUS_REG] >> 6) & ~vi->field;
+    /* toggle vi field if in interlaced mode */
+    vi->field ^= (vi->regs[VI_STATUS_REG] >> 6) & 0x1;
 
     /* schedule next vertical interrupt */
     vi->delay = (vi->regs[VI_V_SYNC_REG] == 0)