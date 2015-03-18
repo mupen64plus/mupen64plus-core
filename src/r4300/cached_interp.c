mupen64plus/mupen64plus-core

commit 36a1c4fff70fde8cbbc81bbaf44e2fe417b3e620

Abstract the r4300 cached code invalidation for uses outside of r4300 subsystem.

src/r4300/cached_interp.c
@@ -585,3 +585,24 @@ void free_blocks(void)
     }
 }
 
+void invalidate_cached_code_hacktarux(uint32_t address, size_t size)
+{
+    size_t i;
+    size_t begin;
+    size_t end;
+
+    if (size == 0)
+    {
+        begin = 0;
+        end = 0xfffff;
+    }
+    else
+    {
+        begin = address >> 12;
+        end = (address+size-1) >> 12;
+    }
+
+    for(i = begin; i <= end; ++i)
+        invalid_code[i] = 1;
+}
+

src/main/savestates.c
@@ -45,7 +45,6 @@
 #include "r4300/cp1.h"
 #include "r4300/r4300.h"
 #include "r4300/r4300_core.h"
-#include "r4300/cached_interp.h"
 #include "osal/preproc.h"
 #include "osd/osd.h"
 #include "pi/pi_controller.h"
@@ -445,26 +444,18 @@ static int savestates_load_m64p(char *filepath)
     }
 
 #ifdef NEW_DYNAREC
-    if (r4300emu == CORE_DYNAREC) {
+    if (r4300emu == CORE_DYNAREC)
+    {
         pcaddr = GETDATA(curr, uint32_t);
         pending_exception = 1;
         invalidate_all_pages();
-    } else {
-        if(r4300emu != CORE_PURE_INTERPRETER)
-        {
-            for (i = 0; i < 0x100000; i++)
-                invalid_code[i] = 1;
-        }
-        generic_jump_to(GETDATA(curr, uint32_t)); // PC
     }
-#else
-    if(r4300emu != CORE_PURE_INTERPRETER)
-    {
-        for (i = 0; i < 0x100000; i++)
-            invalid_code[i] = 1;
-    }
-    generic_jump_to(GETDATA(curr, uint32_t)); // PC
+    else
 #endif
+    {
+        generic_jump_to(GETDATA(curr, uint32_t)); // PC
+        invalidate_r4300_cached_code(0,0);
+    }
 
     next_interupt = GETDATA(curr, unsigned int);
     g_vi.next_vi = GETDATA(curr, unsigned int);
@@ -757,22 +748,13 @@ static int savestates_load_pj64(char *filepath, void *handle,
         pcaddr = last_addr;
         pending_exception = 1;
         invalidate_all_pages();
-    } else {
-        if(r4300emu != CORE_PURE_INTERPRETER)
-        {
-            for (i = 0; i < 0x100000; i++)
-                invalid_code[i] = 1;
-        }
+    }
+    else
+#endif
+    {
+        invalidate_r4300_cached_code(0, 0);
         generic_jump_to(last_addr);
     }
-#else
-    if(r4300emu != CORE_PURE_INTERPRETER)
-    {
-        for (i = 0; i < 0x100000; i++)
-            invalid_code[i] = 1;
-    }
-    generic_jump_to(last_addr);
-#endif
 
     // assert(savestateData+savestateSize == curr)
 

src/memory/memory.c
@@ -27,12 +27,8 @@
 #include "main/main.h"
 #include "main/rom.h"
 
-#include "r4300/r4300.h"
-#include "r4300/r4300_core.h"
-#include "r4300/cached_interp.h"
 #include "r4300/new_dynarec/new_dynarec.h"
-#include "r4300/recomph.h"
-#include "r4300/ops.h"
+#include "r4300/r4300_core.h"
 #include "r4300/tlb.h"
 
 #include "rdp/rdp_core.h"
@@ -48,12 +44,14 @@
 #include "debugger/dbg_types.h"
 #include "debugger/dbg_memory.h"
 #include "debugger/dbg_breakpoints.h"
-
+#include "r4300/r4300.h" /* for PC->addr */
 #include <string.h>
 #endif
 
 #include <stddef.h>
 #include <stdint.h>
+
+extern int fast_memory;
 
 #if NEW_DYNAREC != NEW_DYNAREC_ARM
 // address : address of the read/write operation being done
@@ -169,15 +167,6 @@ static int writed(writefn write_word, void* opaque, uint32_t address, uint64_t v
 }
 
 
-static void invalidate_code(uint32_t address)
-{
-    if (r4300emu != CORE_PURE_INTERPRETER && !invalid_code[address>>12])
-        if (blocks[address>>12]->block[(address&0xFFF)/4].ops !=
-            current_instruction_table.NOTCOMPILED)
-            invalid_code[address>>12] = 1;
-}
-
-
 static void read_nothing(void)
 {
     *rdword = 0;
@@ -244,7 +233,7 @@ static void read_nomemd(void)
 
 static void write_nomem(void)
 {
-    invalidate_code(address);
+    invalidate_r4300_cached_code(address, 4);
     address = virtual_to_physical_address(address,1);
     if (address == 0x00000000) return;
     write_word_in_memory();
@@ -252,7 +241,7 @@ static void write_nomem(void)
 
 static void write_nomemb(void)
 {
-    invalidate_code(address);
+    invalidate_r4300_cached_code(address, 1);
     address = virtual_to_physical_address(address,1);
     if (address == 0x00000000) return;
     write_byte_in_memory();
@@ -260,7 +249,7 @@ static void write_nomemb(void)
 
 static void write_nomemh(void)
 {
-    invalidate_code(address);
+    invalidate_r4300_cached_code(address, 2);
     address = virtual_to_physical_address(address,1);
     if (address == 0x00000000) return;
     write_hword_in_memory();
@@ -268,7 +257,7 @@ static void write_nomemh(void)
 
 static void write_nomemd(void)
 {
-    invalidate_code(address);
+    invalidate_r4300_cached_code(address, 8);
     address = virtual_to_physical_address(address,1);
     if (address == 0x00000000) return;
     write_dword_in_memory();

src/pi/pi_controller.c
@@ -25,10 +25,6 @@
 #include "api/m64p_types.h"
 #include "api/callbacks.h"
 #include "memory/memory.h"
-#include "r4300/cached_interp.h"
-#include "r4300/new_dynarec/new_dynarec.h"
-#include "r4300/ops.h"
-#include "r4300/r4300.h"
 #include "r4300/r4300_core.h"
 #include "ri/rdram_detection_hack.h"
 #include "ri/ri_controller.h"
@@ -65,6 +61,10 @@ static void dma_pi_read(struct pi_controller* pi)
 static void dma_pi_write(struct pi_controller* pi)
 {
     unsigned int longueur, i;
+    uint32_t dram_address;
+    uint32_t rom_address;
+    uint8_t* dram;
+    const uint8_t* rom;
 
     if (pi->regs[PI_CART_ADDR_REG] < 0x10000000)
     {
@@ -120,46 +120,18 @@ static void dma_pi_write(struct pi_controller* pi)
         return;
     }
 
-    if (r4300emu != CORE_PURE_INTERPRETER)
-    {
-        for (i=0; i<longueur; i++)
-        {
-            unsigned long rdram_address1 = pi->regs[PI_DRAM_ADDR_REG]+i+0x80000000;
-            unsigned long rdram_address2 = pi->regs[PI_DRAM_ADDR_REG]+i+0xa0000000;
-            ((unsigned char*)pi->ri->rdram.dram)[(pi->regs[PI_DRAM_ADDR_REG]+i)^S8]=
-                pi->cart_rom.rom[(((pi->regs[PI_CART_ADDR_REG]-0x10000000)&0x3FFFFFF)+i)^S8];
+    dram_address = pi->regs[PI_DRAM_ADDR_REG];
+    rom_address = (pi->regs[PI_CART_ADDR_REG] - 0x10000000) & 0x3ffffff;
+    dram = (uint8_t*)pi->ri->rdram.dram;
+    rom = pi->cart_rom.rom;
 
-            if (!invalid_code[rdram_address1>>12])
-            {
-                if (!blocks[rdram_address1>>12] ||
-                    blocks[rdram_address1>>12]->block[(rdram_address1&0xFFF)/4].ops !=
-                    current_instruction_table.NOTCOMPILED)
-                {
-                    invalid_code[rdram_address1>>12] = 1;
-                }
-#ifdef NEW_DYNAREC
-                invalidate_block(rdram_address1>>12);
-#endif
-            }
-            if (!invalid_code[rdram_address2>>12])
-            {
-                if (!blocks[rdram_address1>>12] ||
-                    blocks[rdram_address2>>12]->block[(rdram_address2&0xFFF)/4].ops !=
-                    current_instruction_table.NOTCOMPILED)
-                {
-                    invalid_code[rdram_address2>>12] = 1;
-                }
-            }
-        }
-    }
-    else
+    for(i = 0; i < longueur; ++i)
     {
-        for (i=0; i<(int)longueur; i++)
-        {
-            ((unsigned char*)pi->ri->rdram.dram)[(pi->regs[PI_DRAM_ADDR_REG]+i)^S8]=
-                pi->cart_rom.rom[(((pi->regs[PI_CART_ADDR_REG]-0x10000000)&0x3FFFFFF)+i)^S8];
-        }
+        dram[(dram_address+i)^S8] = rom[(rom_address+i)^S8];
     }
+
+    invalidate_r4300_cached_code(0x80000000 + dram_address, longueur);
+    invalidate_r4300_cached_code(0xa0000000 + dram_address, longueur);
 
     /* HACK: monitor PI DMA to trigger RDRAM size detection
      * hack just before initial cart ROM loading. */

src/r4300/cached_interp.h
@@ -26,6 +26,9 @@
 /* FIXME: use forward declaration for precomp_block */
 #include "recomp.h"
 
+#include <stddef.h>
+#include <stdint.h>
+
 extern char invalid_code[0x100000];
 extern precomp_block *blocks[0x100000];
 extern precomp_block *actual;
@@ -35,6 +38,8 @@ extern const cpu_instruction_table cached_interpreter_table;
 void init_blocks(void);
 void free_blocks(void);
 void jump_to_func(void);
+
+void invalidate_cached_code_hacktarux(uint32_t address, size_t size);
 
 /* Jumps to the given address. This is for the cached interpreter / dynarec. */
 #define jump_to(a) { jump_to_address = a; jump_to_func(); }

src/r4300/new_dynarec/new_dynarec.c
@@ -1252,6 +1252,27 @@ void invalidate_block(u_int block)
   #endif
 }
 
+void invalidate_cached_code_new_dynarec(uint32_t address, size_t size)
+{
+    size_t i;
+    size_t begin;
+    size_t end;
+
+    if (size == 0)
+    {
+        begin = 0;
+        end = 0xfffff;
+    }
+    else
+    {
+        begin = address >> 12;
+        end = (address+size-1) >> 12;
+    }
+
+    for(i = begin; i <= end; ++i)
+        invalidate_block(i);
+}
+
 #if NEW_DYNAREC == NEW_DYNAREC_ARM
 static void invalidate_addr(u_int addr)
 {

src/r4300/new_dynarec/new_dynarec.h
@@ -22,6 +22,9 @@
 #ifndef M64P_R4300_NEW_DYNAREC_H
 #define M64P_R4300_NEW_DYNAREC_H
 
+#include <stddef.h>
+#include <stdint.h>
+
 #define NEW_DYNAREC_X86 1
 #define NEW_DYNAREC_AMD64 2
 #define NEW_DYNAREC_ARM 3
@@ -37,6 +40,7 @@ extern int pending_exception;
 
 void invalidate_all_pages(void);
 void invalidate_block(unsigned int block);
+void invalidate_cached_code_new_dynarec(uint32_t address, size_t size);
 void new_dynarec_init(void);
 void new_dyna_start(void);
 void new_dynarec_cleanup(void);

src/r4300/r4300_core.c
@@ -21,8 +21,28 @@
 
 #include "r4300_core.h"
 
+#include "cached_interp.h"
+#include "new_dynarec/new_dynarec.h"
+#include "r4300.h"
+
 void init_r4300(struct r4300_core* r4300)
 {
     init_mi(&r4300->mi);
 }
 
+void invalidate_r4300_cached_code(uint32_t address, size_t size)
+{
+    if (r4300emu != CORE_PURE_INTERPRETER)
+    {
+#ifdef NEW_DYNAREC
+        if (r4300emu == CORE_DYNAREC)
+        {
+            invalidate_cached_code_new_dynarec(address, size);
+        }
+        else
+#endif
+        {
+            invalidate_cached_code_hacktarux(address, size);
+        }
+    }
+}

commit 30b97ee9ddfe83959e1fe2e89c6cfef9cc30c058

Don't invalidate cached code if instruction is not compiled.
This fixes the performance regression which affects Gauntlet Legend
when played with the old dynarec. The regression has been introduced
in commit 36a1c4fff70fde8cbbc81bbaf44e2fe417b3e620.

src/r4300/cached_interp.c
@@ -22,6 +22,7 @@
 #include <stdint.h>
 #define __STDC_FORMAT_MACROS
 #include <inttypes.h>
+#include <string.h>
 
 #include "cached_interp.h"
 
@@ -588,21 +589,39 @@ void free_blocks(void)
 void invalidate_cached_code_hacktarux(uint32_t address, size_t size)
 {
     size_t i;
-    size_t begin;
-    size_t end;
+    uint32_t addr;
+    uint32_t addr_max;
 
     if (size == 0)
     {
-        begin = 0;
-        end = 0xfffff;
+        /* invalidate everthing */
+        memset(invalid_code, 1, 0x100000);
     }
     else
     {
-        begin = address >> 12;
-        end = (address+size-1) >> 12;
-    }
+        /* invalidate blocks (if necessary) */
+        addr_max = address+size;
 
-    for(i = begin; i <= end; ++i)
-        invalid_code[i] = 1;
+        for(addr = address; addr < addr_max; addr += 4)
+        {
+            i = (addr >> 12);
+
+            if (invalid_code[i] == 0)
+            {
+                if (blocks[i] == NULL
+                || blocks[i]->block[(addr & 0xfff) / 4].ops != current_instruction_table.NOTCOMPILED)
+                {
+                    invalid_code[i] = 1;
+                    /* go directly to next i */
+                    addr |= 0xffc;
+                }
+            }
+            else
+            {
+                /* go directly to next i */
+                addr |= 0xffc;
+            }
+        }
+    }
 }
 