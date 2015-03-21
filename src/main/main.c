mupen64plus/mupen64plus-audio-sdl

Sort the includes based on type and names

src/main.c
@@ -22,12 +22,11 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+#include <SDL.h>
+#include <SDL_audio.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#include <SDL.h>
-#include <SDL_audio.h>
 
 #ifdef USE_SRC
 #include <samplerate.h>
@@ -42,14 +41,13 @@
 #endif
 
 #define M64P_PLUGIN_PROTOTYPES 1
-#include "m64p_types.h"
-#include "m64p_plugin.h"
 #include "m64p_common.h"
 #include "m64p_config.h"
-
+#include "m64p_plugin.h"
+#include "m64p_types.h"
 #include "main.h"
-#include "volume.h"
 #include "osal_dynamiclib.h"
+#include "volume.h"
 
 /* Default start-time size of primary buffer (in equivalent output samples).
    This is the buffer where audio is loaded after it's extracted from n64's memory.

mupen64plus/mupen64plus-core

src/main/main.c
@@ -28,56 +28,53 @@
  * if you want to implement an interface, you should look here
  */
 
-#include <string.h>
-#include <stdlib.h>
-
 #include <SDL.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define M64P_CORE_PROTOTYPES 1
-#include "api/m64p_types.h"
+#include "ai/ai_controller.h"
 #include "api/callbacks.h"
 #include "api/config.h"
-#include "api/m64p_config.h"
 #include "api/debugger.h"
+#include "api/m64p_config.h"
+#include "api/m64p_types.h"
 #include "api/m64p_vidext.h"
 #include "api/vidext.h"
-
-#include "main.h"
 #include "cheat.h"
 #include "eep_file.h"
 #include "eventloop.h"
 #include "fla_file.h"
-#include "mpk_file.h"
-#include "profile.h"
-#include "rom.h"
-#include "savestates.h"
-#include "sra_file.h"
-#include "util.h"
-
-#include "ai/ai_controller.h"
+#include "main.h"
 #include "memory/memory.h"
+#include "mpk_file.h"
 #include "osal/files.h"
 #include "osal/preproc.h"
 #include "osd/osd.h"
 #include "osd/screenshot.h"
 #include "pi/pi_controller.h"
-#include "plugin/plugin.h"
 #include "plugin/emulate_game_controller_via_input_plugin.h"
 #include "plugin/emulate_speaker_via_audio_plugin.h"
 #include "plugin/get_time_using_C_localtime.h"
+#include "plugin/plugin.h"
 #include "plugin/rumble_via_input_plugin.h"
+#include "profile.h"
 #include "r4300/r4300.h"
 #include "r4300/r4300_core.h"
 #include "r4300/reset.h"
 #include "rdp/rdp_core.h"
 #include "ri/ri_controller.h"
+#include "rom.h"
 #include "rsp/rsp_core.h"
+#include "savestates.h"
 #include "si/si_controller.h"
+#include "sra_file.h"
+#include "util.h"
 #include "vi/vi_controller.h"
 
 #ifdef DBG
-#include "debugger/dbg_types.h"
 #include "debugger/dbg_debugger.h"
+#include "debugger/dbg_types.h"
 #endif
 
 #ifdef WITH_LIRC