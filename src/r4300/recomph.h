6d0a99f54d9841206c29a7f248cc6a940cbc7440
Gillou68310 <gilles_siberlin@hotmail.fr>
2015-09-01 11:04:00 +0200

    old_dynarec: Use uintptr_t instead of using macros to differentiate between "long" and "long long" in gencallinterp()

--- src/r4300/recomph.h
+++ src/r4300/recomph.h
@@ -41,11 +41,7 @@ void passe2(precomp_instr *dest, int start, int end, precomp_block* block);
 void init_assembler(void *block_jumps_table, int block_jumps_number, void *block_riprel_table, int block_riprel_number);
 void free_assembler(void **block_jumps_table, int *block_jumps_number, void **block_riprel_table, int *block_riprel_number);
 
-#if defined(__x86_64__)
-void gencallinterp(unsigned long long addr, int jump);
-#else
-void gencallinterp(unsigned long addr, int jump);
-#endif
+void gencallinterp(uintptr_t addr, int jump);
 
 void genupdate_system(int type);
 void genbnel(void);
