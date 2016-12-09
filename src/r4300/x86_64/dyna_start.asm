ba554ea278d3bbd05ad194a6d6b1898eed511c07
Bobby Smiles <bobby.smiles32@gmail.com>
2016-12-09 07:54:13 +0100

    Generate asm_defines compatible with gas. Use it in linkage_arm.S

--- src/r4300/x86_64/dyna_start.asm
+++ src/r4300/x86_64/dyna_start.asm
@@ -18,7 +18,7 @@
 ;Free Software Foundation, Inc.,
 ;51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-%include "../../src/main/asm_defines.h"
+%include "../../src/main/asm_defines_nasm.h"
 
 global dyna_start
 
