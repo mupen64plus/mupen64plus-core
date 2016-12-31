85e0773ba6c138b464a2419c028e236e0c05da3a
Bobby Smiles <bobby.smiles32@gmail.com>
2016-12-31 15:28:23 +0100

    Use ASFLAGS to simplify include dirs

--- src/r4300/new_dynarec/x86/linkage_x86.asm
+++ src/r4300/new_dynarec/x86/linkage_x86.asm
@@ -16,7 +16,7 @@
 ;Free Software Foundation, Inc.,                                       
 ;51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          
 
-%include "../../../../src/main/asm_defines_nasm.h"
+%include "main/asm_defines_nasm.h"
 
 %ifdef ELF_TYPE
     %macro  cglobal 1
