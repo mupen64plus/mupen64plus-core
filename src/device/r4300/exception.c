eff09046a499267f4f91e62dde6bcc8a28b4033a
Bobby Smiles <bobby.smiles32@gmail.com>
2017-03-02 15:30:20 +0100

    Formattings in exception.c

--- src/device/r4300/exception.c
+++ src/device/r4300/exception.c
@@ -31,118 +31,138 @@
 
 void TLB_refill_exception(uint32_t address, int w)
 {
-   uint32_t* cp0_regs = r4300_cp0_regs();
-   int usual_handler = 0, i;
+    uint32_t* cp0_regs = r4300_cp0_regs();
+    int usual_handler = 0, i;
 
-   if (g_dev.r4300.emumode != EMUMODE_DYNAREC && w != 2) cp0_update_count();
-   if (w == 1) cp0_regs[CP0_CAUSE_REG] = CP0_CAUSE_EXCCODE_TLBS;
-   else cp0_regs[CP0_CAUSE_REG] = CP0_CAUSE_EXCCODE_TLBL;
-   cp0_regs[CP0_BADVADDR_REG] = address;
-   cp0_regs[CP0_CONTEXT_REG] = (cp0_regs[CP0_CONTEXT_REG] & UINT32_C(0xFF80000F)) | ((address >> 9) & UINT32_C(0x007FFFF0));
-   cp0_regs[CP0_ENTRYHI_REG] = address & UINT32_C(0xFFFFE000);
-   if (cp0_regs[CP0_STATUS_REG] & CP0_STATUS_EXL)
-     {
-    generic_jump_to(&g_dev.r4300, UINT32_C(0x80000180));
-    if(g_dev.r4300.delay_slot==1 || g_dev.r4300.delay_slot==3) cp0_regs[CP0_CAUSE_REG] |= CP0_CAUSE_BD;
-    else cp0_regs[CP0_CAUSE_REG] &= ~CP0_CAUSE_BD;
-     }
-   else
-     {
-    if (g_dev.r4300.emumode != EMUMODE_PURE_INTERPRETER) 
-      {
-         if (w!=2)
-           cp0_regs[CP0_EPC_REG] = *r4300_pc();
-         else
-           cp0_regs[CP0_EPC_REG] = address;
-      }
-    else cp0_regs[CP0_EPC_REG] = *r4300_pc();
-         
-    cp0_regs[CP0_CAUSE_REG] &= ~CP0_CAUSE_BD;
-    cp0_regs[CP0_STATUS_REG] |= CP0_STATUS_EXL;
-    
-    if (address >= UINT32_C(0x80000000) && address < UINT32_C(0xc0000000))
-      usual_handler = 1;
-    for (i=0; i<32; i++)
-      {
-         if (/*g_dev.r4300.cp0.tlb.entries[i].v_even &&*/ address >= g_dev.r4300.cp0.tlb.entries[i].start_even &&
-         address <= g_dev.r4300.cp0.tlb.entries[i].end_even)
-           usual_handler = 1;
-         if (/*g_dev.r4300.cp0.tlb.entries[i].v_odd &&*/ address >= g_dev.r4300.cp0.tlb.entries[i].start_odd &&
-         address <= g_dev.r4300.cp0.tlb.entries[i].end_odd)
-           usual_handler = 1;
-      }
-    if (usual_handler)
-      {
-         generic_jump_to(&g_dev.r4300, UINT32_C(0x80000180));
-      }
+    if (g_dev.r4300.emumode != EMUMODE_DYNAREC && w != 2) {
+        cp0_update_count();
+    }
+
+    cp0_regs[CP0_CAUSE_REG] = (w == 1)
+        ? CP0_CAUSE_EXCCODE_TLBS
+        : CP0_CAUSE_EXCCODE_TLBL;
+
+    cp0_regs[CP0_BADVADDR_REG] = address;
+    cp0_regs[CP0_CONTEXT_REG] = (cp0_regs[CP0_CONTEXT_REG] & UINT32_C(0xFF80000F))
+        | ((address >> 9) & UINT32_C(0x007FFFF0));
+    cp0_regs[CP0_ENTRYHI_REG] = address & UINT32_C(0xFFFFE000);
+
+    if (cp0_regs[CP0_STATUS_REG] & CP0_STATUS_EXL)
+    {
+        generic_jump_to(&g_dev.r4300, UINT32_C(0x80000180));
+
+
+        if (g_dev.r4300.delay_slot == 1 || g_dev.r4300.delay_slot == 3) {
+            cp0_regs[CP0_CAUSE_REG] |= CP0_CAUSE_BD;
+        }
+        else {
+            cp0_regs[CP0_CAUSE_REG] &= ~CP0_CAUSE_BD;
+        }
+    }
     else
-      {
-         generic_jump_to(&g_dev.r4300, UINT32_C(0x80000000));
-      }
-     }
-   if(g_dev.r4300.delay_slot==1 || g_dev.r4300.delay_slot==3)
-     {
-    cp0_regs[CP0_CAUSE_REG] |= CP0_CAUSE_BD;
-    cp0_regs[CP0_EPC_REG]-=4;
-     }
-   else
-     {
-    cp0_regs[CP0_CAUSE_REG] &= ~CP0_CAUSE_BD;
-     }
-   if(w != 2) cp0_regs[CP0_EPC_REG]-=4;
-   
-   g_dev.r4300.cp0.last_addr = *r4300_pc();
-   
-   if (g_dev.r4300.emumode == EMUMODE_DYNAREC) 
-     {
-    dyna_jump();
-    if (!g_dev.r4300.dyna_interp) g_dev.r4300.delay_slot = 0;
-     }
-   
-   if (g_dev.r4300.emumode != EMUMODE_DYNAREC || g_dev.r4300.dyna_interp)
-     {
-    g_dev.r4300.dyna_interp = 0;
-    if (g_dev.r4300.delay_slot)
-      {
-         g_dev.r4300.skip_jump = *r4300_pc();
-         *r4300_cp0_next_interrupt() = 0;
-      }
-     }
+    {
+        if (g_dev.r4300.emumode != EMUMODE_PURE_INTERPRETER)
+        {
+            cp0_regs[CP0_EPC_REG] = (w != 2)
+                ? *r4300_pc()
+                : address;
+        }
+        else {
+            cp0_regs[CP0_EPC_REG] = *r4300_pc();
+        }
+
+        cp0_regs[CP0_CAUSE_REG] &= ~CP0_CAUSE_BD;
+        cp0_regs[CP0_STATUS_REG] |= CP0_STATUS_EXL;
+
+        if (address >= UINT32_C(0x80000000) && address < UINT32_C(0xc0000000)) {
+            usual_handler = 1;
+        }
+
+        for (i = 0; i < 32; i++)
+        {
+            if (/*g_dev.r4300.cp0.tlb.entries[i].v_even &&*/ address >= g_dev.r4300.cp0.tlb.entries[i].start_even &&
+                    address <= g_dev.r4300.cp0.tlb.entries[i].end_even) {
+                usual_handler = 1;
+            }
+            if (/*g_dev.r4300.cp0.tlb.entries[i].v_odd &&*/ address >= g_dev.r4300.cp0.tlb.entries[i].start_odd &&
+                    address <= g_dev.r4300.cp0.tlb.entries[i].end_odd) {
+                usual_handler = 1;
+            }
+        }
+
+        generic_jump_to(&g_dev.r4300, (usual_handler)
+                ? UINT32_C(0x80000180)
+                : UINT32_C(0x80000000));
+    }
+
+    if (g_dev.r4300.delay_slot == 1 || g_dev.r4300.delay_slot == 3)
+    {
+        cp0_regs[CP0_CAUSE_REG] |= CP0_CAUSE_BD;
+        cp0_regs[CP0_EPC_REG] -= 4;
+    }
+    else
+    {
+        cp0_regs[CP0_CAUSE_REG] &= ~CP0_CAUSE_BD;
+    }
+    if (w != 2) {
+        cp0_regs[CP0_EPC_REG] -= 4;
+    }
+
+    g_dev.r4300.cp0.last_addr = *r4300_pc();
+
+    if (g_dev.r4300.emumode == EMUMODE_DYNAREC)
+    {
+        dyna_jump();
+        if (!g_dev.r4300.dyna_interp) { g_dev.r4300.delay_slot = 0; }
+    }
+
+    if (g_dev.r4300.emumode != EMUMODE_DYNAREC || g_dev.r4300.dyna_interp)
+    {
+        g_dev.r4300.dyna_interp = 0;
+        if (g_dev.r4300.delay_slot)
+        {
+            g_dev.r4300.skip_jump = *r4300_pc();
+            *r4300_cp0_next_interrupt() = 0;
+        }
+    }
 }
 
 void exception_general(void)
 {
-   uint32_t* cp0_regs = r4300_cp0_regs();
+    uint32_t* cp0_regs = r4300_cp0_regs();
 
-   cp0_update_count();
-   cp0_regs[CP0_STATUS_REG] |= CP0_STATUS_EXL;
-   
-   cp0_regs[CP0_EPC_REG] = *r4300_pc();
-   
-   if(g_dev.r4300.delay_slot==1 || g_dev.r4300.delay_slot==3)
-     {
-    cp0_regs[CP0_CAUSE_REG] |= CP0_CAUSE_BD;
-    cp0_regs[CP0_EPC_REG]-=4;
-     }
-   else
-     {
-    cp0_regs[CP0_CAUSE_REG] &= ~CP0_CAUSE_BD;
-     }
-   generic_jump_to(&g_dev.r4300, UINT32_C(0x80000180));
-   g_dev.r4300.cp0.last_addr = *r4300_pc();
-   if (g_dev.r4300.emumode == EMUMODE_DYNAREC)
-     {
-    dyna_jump();
-    if (!g_dev.r4300.dyna_interp) g_dev.r4300.delay_slot = 0;
-     }
-   if (g_dev.r4300.emumode != EMUMODE_DYNAREC || g_dev.r4300.dyna_interp)
-     {
-    g_dev.r4300.dyna_interp = 0;
-    if (g_dev.r4300.delay_slot)
-      {
-         g_dev.r4300.skip_jump = *r4300_pc();
-         *r4300_cp0_next_interrupt() = 0;
-      }
-     }
+    cp0_update_count();
+    cp0_regs[CP0_STATUS_REG] |= CP0_STATUS_EXL;
+
+    cp0_regs[CP0_EPC_REG] = *r4300_pc();
+
+    if (g_dev.r4300.delay_slot == 1 || g_dev.r4300.delay_slot == 3)
+    {
+        cp0_regs[CP0_CAUSE_REG] |= CP0_CAUSE_BD;
+        cp0_regs[CP0_EPC_REG] -= 4;
+    }
+    else
+    {
+        cp0_regs[CP0_CAUSE_REG] &= ~CP0_CAUSE_BD;
+    }
+
+    generic_jump_to(&g_dev.r4300, UINT32_C(0x80000180));
+
+    g_dev.r4300.cp0.last_addr = *r4300_pc();
+
+    if (g_dev.r4300.emumode == EMUMODE_DYNAREC)
+    {
+        dyna_jump();
+        if (!g_dev.r4300.dyna_interp) { g_dev.r4300.delay_slot = 0; }
+    }
+    if (g_dev.r4300.emumode != EMUMODE_DYNAREC || g_dev.r4300.dyna_interp)
+    {
+        g_dev.r4300.dyna_interp = 0;
+        if (g_dev.r4300.delay_slot)
+        {
+            g_dev.r4300.skip_jump = *r4300_pc();
+            *r4300_cp0_next_interrupt() = 0;
+        }
+    }
 }
 
