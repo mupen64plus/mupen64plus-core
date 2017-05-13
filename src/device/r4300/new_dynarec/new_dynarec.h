3c584df915684f5e55939785bd6384b276c29459
Bobby Smiles <bobby.smiles32@gmail.com>
2017-05-13 20:34:30 +0200

    Fix pointer types for cp1 registers.

--- src/device/r4300/new_dynarec/new_dynarec.h
+++ src/device/r4300/new_dynarec/new_dynarec.h
@@ -76,8 +76,8 @@ struct new_dynarec_hot_state
     int64_t  hi;
     int64_t  lo;
     uint32_t cp0_regs[32];
-    uint32_t cp1_regs_simple[32];
-    uint32_t cp1_regs_double[32];
+    float* cp1_regs_simple[32];
+    double* cp1_regs_double[32];
     uint32_t rounding_modes[4];
     int branch_target;
     struct precomp_instr* pc;
