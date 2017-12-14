mupen64plus/mupen64plus-core

commit 1c01c2339bafb503e46cf776d995e51ca46c73b4

Rewrite pifbootrom to avoid usage of device struct.

src/device/r4300/interrupt.c
@@ -435,7 +435,7 @@ void nmi_int_handler(void* opaque)
     cp0_regs[CP0_STATUS_REG] = (cp0_regs[CP0_STATUS_REG] & ~(CP0_STATUS_SR | CP0_STATUS_TS | UINT32_C(0x00080000))) | (CP0_STATUS_ERL | CP0_STATUS_BEV | CP0_STATUS_SR);
     cp0_regs[CP0_CAUSE_REG]  = 0x00000000;
     // simulate the soft reset code which would run from the PIF ROM
-    pifbootrom_hle_execute(dev);
+    pifbootrom_hle_execute(r4300);
     // clear all interrupts, reset interrupt counters back to 0
     cp0_regs[CP0_COUNT_REG] = 0;
     g_gs_vi_counter = 0;
@@ -483,7 +483,7 @@ void reset_hard_handler(void* opaque)
 
     poweron_device(dev);
 
-    pifbootrom_hle_execute(dev);
+    pifbootrom_hle_execute(r4300);
     r4300->cp0.last_addr = UINT32_C(0xa4000040);
     *r4300_cp0_next_interrupt(&r4300->cp0) = 624999;
     init_interrupt(&r4300->cp0);

src/device/si/pif.c
@@ -308,6 +308,20 @@ void poweron_pif(struct pif* pif)
 
     memset(pif->ram, 0, PIF_RAM_SIZE);
 
+    /* HACK: for allowing pifbootrom execution */
+    unsigned int rom_type = 0;
+    unsigned int reset_type = 0;
+    unsigned int s7 = 0;
+
+    uint32_t* pif24 = (uint32_t*)(pif->ram + 0x24);
+    *pif24 = (uint32_t)
+         (((rom_type      & 0x1) << 19)
+        | ((s7            & 0x1) << 18)
+        | ((reset_type    & 0x1) << 17)
+        | ((pif->cic.seed & 0xff) << 8)
+        | 0x3f);
+    *pif24 = sl(*pif24);
+
     for(i = 0; i < PIF_CHANNELS_COUNT; ++i) {
         disable_pif_channel(&pif->channels[i]);
     }

src/device/ai/ai_controller.c
@@ -195,7 +195,12 @@ void write_ai_regs(void* opaque, uint32_t address, uint32_t value, uint32_t mask
     {
     case AI_LEN_REG:
         masked_write(&ai->regs[AI_LEN_REG], value, mask);
-        fifo_push(ai);
+        if (ai->regs[AI_LEN_REG] != 0) {
+            fifo_push(ai);
+        }
+        else {
+            /* stop sound */
+        }
         return;
 
     case AI_STATUS_REG:

src/device/device.h
@@ -56,6 +56,7 @@ struct storage_backend;
 #define MM_RI_REGS          UINT32_C(0x04700000)
 #define MM_SI_REGS          UINT32_C(0x04800000)
 #define MM_DD_REGS          UINT32_C(0x05000000) /* dom2 addr1 */
+#define MM_DD_ROM           UINT32_C(0x06000000) /* dom1 addr1 */
 #define MM_FLASHRAM_STATUS  UINT32_C(0x08000000) /* dom2 addr2 */
 #define MM_FLASHRAM_COMMAND UINT32_C(0x08010000)
 #define MM_CART_ROM         UINT32_C(0x10000000) /* dom1 addr2 */

src/device/pifbootrom/pifbootrom.c
@@ -46,63 +46,98 @@ static unsigned int get_tv_type(void)
     }
 }
 
-void pifbootrom_hle_execute(struct device* dev)
+void pifbootrom_hle_execute(struct r4300_core* r4300)
 {
-    unsigned int rom_type = 0;              /* 0:Cart, 1:DD */
-    unsigned int reset_type = 0;            /* 0:ColdReset, 1:NMI */
-    unsigned int s7 = 0;                    /* ??? */
+    uint32_t pif24;
+    unsigned int seed;       /* seed (depends on CIC version) */
+    unsigned int rom_type;   /* 0:Cart, 1:DD */
+    unsigned int reset_type; /* 0:ColdReset, 1:NMI */
+    unsigned int s7;         /* ??? */
+    uint32_t bsd_dom1_config;
     unsigned int tv_type = get_tv_type();   /* 0:PAL, 1:NTSC, 2:MPAL */
-    uint32_t bsd_dom1_config = *(uint32_t*)dev->pi.cart_rom.rom;
 
-    int64_t* r4300_gpregs = r4300_regs(&dev->r4300);
-    uint32_t* cp0_regs = r4300_cp0_regs(&dev->r4300.cp0);
+    int64_t* r4300_gpregs = r4300_regs(r4300);
+    uint32_t* cp0_regs = r4300_cp0_regs(&r4300->cp0);
 
     /* setup CP0 registers */
     cp0_regs[CP0_STATUS_REG] = 0x34000000;
     cp0_regs[CP0_CONFIG_REG] = 0x0006e463;
 
-    /* stop RSP */
-    dev->sp.regs[SP_STATUS_REG] = 1;
-    dev->sp.regs2[SP_PC_REG] = 0;
+    /* XXX: wait for SP to finish last operation (poll halt) */
 
-    /* stop PI, configure ROM access */
-    dev->pi.regs[PI_BSD_DOM1_LAT_REG] = (bsd_dom1_config      ) & 0xff;
-    dev->pi.regs[PI_BSD_DOM1_PWD_REG] = (bsd_dom1_config >>  8) & 0xff;
-    dev->pi.regs[PI_BSD_DOM1_PGS_REG] = (bsd_dom1_config >> 16) & 0x0f;
-    dev->pi.regs[PI_BSD_DOM1_RLS_REG] = (bsd_dom1_config >> 20) & 0x03;
-    dev->pi.regs[PI_STATUS_REG] = 0;
+    /* stop RSP (halt + clear interrupts) */
+    r4300_write_aligned_word(r4300, R4300_KSEG1 + MM_RSP_REGS  + 4*SP_STATUS_REG, 0x0a, ~UINT32_C(0));
 
-    /* mute sound */
-    dev->ai.regs[AI_DRAM_ADDR_REG] = 0;
-    dev->ai.regs[AI_LEN_REG] = 0;
+    /* XXX: wait for SP DMA to finish (poll busy) */
+
+    /* stop PI (reset, clear interrupt) */
+    r4300_write_aligned_word(r4300, R4300_KSEG1 + MM_PI_REGS + 4*PI_STATUS_REG, 0x3, ~UINT32_C(0));
 
     /* blank screen */
-    dev->vi.regs[VI_V_INTR_REG] = 1023;
-    dev->vi.regs[VI_CURRENT_REG] = 0;
-    dev->vi.regs[VI_H_START_REG] = 0;
+    r4300_write_aligned_word(r4300, R4300_KSEG1 + MM_VI_REGS + 4*VI_V_INTR_REG, 1023, ~UINT32_C(0));
+    r4300_write_aligned_word(r4300, R4300_KSEG1 + MM_VI_REGS + 4*VI_CURRENT_REG,   0, ~UINT32_C(0));
+    r4300_write_aligned_word(r4300, R4300_KSEG1 + MM_VI_REGS + 4*VI_H_START_REG,   0, ~UINT32_C(0));
 
-    /* clear RCP interrupts */
-    dev->r4300.mi.regs[MI_INTR_REG] &= ~(MI_INTR_PI | MI_INTR_VI | MI_INTR_AI | MI_INTR_SP);
+    /* mute sound */
+    r4300_write_aligned_word(r4300, R4300_KSEG1 + MM_AI_REGS + 4*AI_DRAM_ADDR_REG, 0, ~UINT32_C(0));
+    r4300_write_aligned_word(r4300, R4300_KSEG1 + MM_AI_REGS + 4*AI_LEN_REG,       0, ~UINT32_C(0));
+
+    /* XXX: wait for SP DMA to finish (poll busy) */
+
+    /* XXX: copy IPL2 to IMEM (partialy done later as required by CIC x105)
+     * and transfer execution to IPL2 */
+
+    /* XXX: wait for PIF_3c[7] to be cleared */
+
+    /* read and parse pif24 */
+    r4300_read_aligned_word(r4300, R4300_KSEG1 + MM_PIF_MEM + 0x7c0 + 0x24, &pif24);
+    rom_type   = (pif24 >> 19) & 0x01;
+    s7         = (pif24 >> 18) & 0x01;
+    reset_type = (pif24 >> 17) & 0x01;
+    seed       = (pif24 >>  8) & 0xff;
+    /* setup s3-s7 registers (needed by OS) */
+    r4300_gpregs[19] = rom_type;    /* s3 */
+    r4300_gpregs[20] = tv_type;     /* s4 */
+    r4300_gpregs[21] = reset_type;  /* s5 */
+    r4300_gpregs[22] = seed;        /* s6 */
+    r4300_gpregs[23] = s7;          /* s7 */
+
+
+    /* XXX: wait for SI_RD_BUSY to be cleared, set PIF_3c[4] */
+
+    /* configure ROM access
+     * XXX: we skip the first temporary configuration */
+    uint32_t rom_base = (rom_type == 0) ? MM_CART_ROM : MM_DD_ROM;
+    r4300_read_aligned_word(r4300, R4300_KSEG1 + rom_base, &bsd_dom1_config);
+    r4300_write_aligned_word(r4300, R4300_KSEG1 + MM_PI_REGS + 4*PI_BSD_DOM1_LAT_REG, (bsd_dom1_config      ) & 0xff, ~UINT32_C(0));
+    r4300_write_aligned_word(r4300, R4300_KSEG1 + MM_PI_REGS + 4*PI_BSD_DOM1_PWD_REG, (bsd_dom1_config >>  8) & 0xff, ~UINT32_C(0));
+    r4300_write_aligned_word(r4300, R4300_KSEG1 + MM_PI_REGS + 4*PI_BSD_DOM1_PGS_REG, (bsd_dom1_config >> 16) & 0x0f, ~UINT32_C(0));
+    r4300_write_aligned_word(r4300, R4300_KSEG1 + MM_PI_REGS + 4*PI_BSD_DOM1_RLS_REG, (bsd_dom1_config >> 20) & 0x03, ~UINT32_C(0));
+
+    /* XXX: if XBus is used, wait until DPC_pipe_busy is cleared */
 
     /* copy IPL3 to dmem */
-    memcpy((unsigned char*)dev->sp.mem+0x40, dev->pi.cart_rom.rom+0x40, 0xfc0);
+    memcpy((unsigned char*)r4300->mem->base + MM_RSP_MEM + 0x40,
+           (unsigned char*)r4300->mem->base + rom_base + 0x40,
+           0xfc0);
 
-    /* setup s3-s7 registers (needed by OS) */
-    r4300_gpregs[19] = rom_type;     /* s3 */
-    r4300_gpregs[20] = tv_type;      /* s4 */
-    r4300_gpregs[21] = reset_type;   /* s5 */
-    r4300_gpregs[22] = dev->si.pif.cic.seed;/* s6 */
-    r4300_gpregs[23] = s7;           /* s7 */
+    /* XXX: compute IPL3 checksum */
+    /* XXX: wait for SI_RD_BUSY to be cleared, set PIF_30 */
+    /* XXX: wait for SI_RD_BUSY to be cleared, set PIF_34 */
+    /* XXX: wait for SI_RD_BUSY to be cleared, set PIF_3c[5] */
+    /* XXX: wait for PIF_3c[7] to be set */
+    /* XXX: wait for SI_RD_BUSY to be cleared, set PIF_3c[6] */
 
     /* required by CIC x105 */
-    dev->sp.mem[0x1000/4] = 0x3c0dbfc0;
-    dev->sp.mem[0x1004/4] = 0x8da807fc;
-    dev->sp.mem[0x1008/4] = 0x25ad07c0;
-    dev->sp.mem[0x100c/4] = 0x31080080;
-    dev->sp.mem[0x1010/4] = 0x5500fffc;
-    dev->sp.mem[0x1014/4] = 0x3c0dbfc0;
-    dev->sp.mem[0x1018/4] = 0x8da80024;
-    dev->sp.mem[0x101c/4] = 0x3c0bb000;
+    uint32_t* imem = (uint32_t*)((uint8_t*)r4300->mem->base + MM_RSP_MEM + 0x1000);
+    imem[0x0000/4] = 0x3c0dbfc0;
+    imem[0x0004/4] = 0x8da807fc;
+    imem[0x0008/4] = 0x25ad07c0;
+    imem[0x000c/4] = 0x31080080;
+    imem[0x0010/4] = 0x5500fffc;
+    imem[0x0014/4] = 0x3c0dbfc0;
+    imem[0x0018/4] = 0x8da80024;
+    imem[0x001c/4] = 0x3c0bb000;
 
     /* required by CIC x105 */
     r4300_gpregs[11] = INT64_C(0xffffffffa4000040); /* t3 */
@@ -111,5 +146,5 @@ void pifbootrom_hle_execute(struct device* dev)
 
     /* XXX: should prepare execution of IPL3 in DMEM here :
      * e.g. jump to 0xa4000040 */
-    *r4300_cp0_last_addr(&dev->r4300.cp0) = 0xa4000040;
+    *r4300_cp0_last_addr(&r4300->cp0) = 0xa4000040;
 }

src/device/pifbootrom/pifbootrom.h
@@ -22,10 +22,10 @@
 #ifndef M64P_DEVICE_PIFBOOTROM_PIFBOOTROM_H
 #define M64P_DEVICE_PIFBOOTROM_PIFBOOTROM_H
 
-struct device;
+struct r4300_core;
 
 /* Simulates end result of PIFBootROM execution.
  * This bypasses entirely the execution of real PIFBootROM */
-void pifbootrom_hle_execute(struct device* dev);
+void pifbootrom_hle_execute(struct r4300_core* r4300);
 
 #endif

src/device/r4300/r4300_core.h
@@ -206,6 +206,9 @@ struct r4300_core
     uint32_t special_rom;
 };
 
+#define R4300_KSEG0 UINT32_C(0x80000000)
+#define R4300_KSEG1 UINT32_C(0xa0000000)
+
 void init_r4300(struct r4300_core* r4300, struct memory* mem, struct ri_controller* ri, const struct interrupt_handler* interrupt_handlers, unsigned int emumode, unsigned int count_per_op, int no_compiled_jump, int special_rom);
 void poweron_r4300(struct r4300_core* r4300);
 

src/main/main.c
@@ -1147,7 +1147,7 @@ m64p_error main_run(void)
     StateChanged(M64CORE_EMU_STATE, M64EMU_RUNNING);
 
     poweron_device(&g_dev);
-    pifbootrom_hle_execute(&g_dev);
+    pifbootrom_hle_execute(&g_dev.r4300);
     run_device(&g_dev);
 
     /* now begin to shut down */

commit ed421be9e7bd8b3e1cdad50f982f355b96a9485e

Fix reset bug introduced in commit 1c01c233.
Values comming from CIC at poweron/reset were written too early in PIF
RAM. There was a possibility of them getting overwritten by some SI DMA
requests (such as reading mempaks [Perfect Dark]) during the time window
between PIF_INT and NMI_INT.

Now we delay this until NMI_INT is triggered.

src/device/device.c
@@ -243,5 +243,7 @@ void hard_reset_device(struct device* dev)
 
 void soft_reset_device(struct device* dev)
 {
-    reset_pif(&dev->pif, 1); /* Soft reset */
+    /* schedule HW2 interrupt now and an NMI after 1/2 seconds */
+    add_interrupt_event(&dev->r4300.cp0, HW2_INT, 0);
+    add_interrupt_event(&dev->r4300.cp0, NMI_INT, 50000000);
 }

src/device/pif/pif.c
@@ -166,12 +166,6 @@ void reset_pif(struct pif* pif, unsigned int reset_type)
 
     /* clear PIF flags */
     pif->ram[0x3f] = 0x00;
-
-    if (reset_type) {
-        /* schedule HW2 interrupt now and an NMI after 1/2 seconds */
-        add_interrupt_event(&pif->r4300->cp0, HW2_INT, 0);
-        add_interrupt_event(&pif->r4300->cp0, NMI_INT, 50000000);
-    }
 }
 
 void setup_channels_format(struct pif* pif)

src/device/r4300/interrupt.c
@@ -439,6 +439,8 @@ void nmi_int_handler(void* opaque)
     struct r4300_core* r4300 = &dev->r4300;
     uint32_t* cp0_regs = r4300_cp0_regs(&r4300->cp0);
 
+    reset_pif(&dev->pif, 1);
+
     // setup r4300 Status flags: reset TS and SR, set BEV, ERL, and SR
     cp0_regs[CP0_STATUS_REG] = (cp0_regs[CP0_STATUS_REG] & ~(CP0_STATUS_SR | CP0_STATUS_TS | UINT32_C(0x00080000))) | (CP0_STATUS_ERL | CP0_STATUS_BEV | CP0_STATUS_SR);
     cp0_regs[CP0_CAUSE_REG]  = 0x00000000;