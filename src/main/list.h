322315499307e50a345f06b00c262a1d600c251d
Bobby Smiles <bobby.smiles32@gmail.com>
2018-08-28 00:47:26 +0200

    Prefer __typeof__ instead of typeof to avoid erros with ISO C compilers.

--- src/main/list.h
+++ src/main/list.h
@@ -83,7 +83,7 @@ static osal_inline int list_empty(const struct list_head *head)
 #ifdef __GNUC__
 
 #define container_of(ptr, type, member) __extension__ ({ \
-    const typeof( ((type *)0)->member ) *__mptr = (ptr); \
+    const __typeof__( ((type *)0)->member ) *__mptr = (ptr); \
     (type *)( (char *)__mptr - offsetof(type,member) );})
 
 #else
