9ab971e750ce8f8b4a0c68f40f96df5a1811264c
Gillou68310 <gilles.siberlin@outlook.com>
2021-06-22 15:25:21 +0200

    new_dynarec: Check CP0_STATUS_REG before float jump in case jump and delay slot instructions span two pages

--- src/device/r4300/new_dynarec/new_dynarec.c
+++ src/device/r4300/new_dynarec/new_dynarec.c
@@ -7507,9 +7507,17 @@ static void pagespan_assemble(int i,struct regstat *i_regs)
   }
   assert(opcode[i]!=1); // BLTZ/BGEZ
 
-  //FIXME: Check CSREG
   if(opcode[i]==0x11 && opcode2[i]==0x08 ) {
-    assert(0);
+    // Check cop1 unusable
+    if(!cop1_usable) {
+      signed char cs=get_reg(i_regs->regmap,CSREG);
+      assert(cs>=0);
+      emit_testimm(cs,CP0_STATUS_CU1);
+      intptr_t jaddr=(intptr_t)out;
+      emit_jeq(0);
+      add_stub(FP_STUB,jaddr,(intptr_t)out,i,cs,(intptr_t)i_regs,0,0);
+      cop1_usable=1;
+    }
     if((source[i]&0x30000)==0) // BC1F
     {
       emit_mov2imm_compact(ba[i],addr,start+i*4+8,alt);
