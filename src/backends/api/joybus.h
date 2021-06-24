aeda4daedb2931ef3d84d87b77c0a102d0e8f2de
Christopher Bonhage <me@christopherbonhage.com>
2021-06-24 08:50:43 -0400

    Revert byte swap on EEPROM Joybus identifiers

--- src/backends/api/joybus.h
+++ src/backends/api/joybus.h
@@ -45,8 +45,8 @@ enum joybus_device_types
     JDT_JOY_PORT         = 0x0004,  /* has port for external paks */
     JDT_AF_RTC           = 0x1000,  /* RTC */
     JDT_EEPROM_NONE      = 0x0000,  /* EEPROM not present */
-    JDT_EEPROM_4K        = 0x0080,  /* 4k EEPROM */
-    JDT_EEPROM_16K       = 0x00c0,  /* 16k EEPROM */
+    JDT_EEPROM_4K        = 0x8000,  /* 4k EEPROM */
+    JDT_EEPROM_16K       = 0xc000,  /* 16k EEPROM */
 };
 
 /* snippet which helps validate command format */
