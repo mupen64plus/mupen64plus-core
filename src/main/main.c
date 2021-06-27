f95c95feb4de649644cee00f7d3c74bd91bf815f
carnivoroussociety <38839485+carnivoroussociety@users.noreply.github.com>
2021-06-27 15:04:03 +1000

    Turned overclock limit up to 11
    Shows improvements for games like GoldenEye 007 and Perfect Dark
    Funny reference bonus

--- src/main/main.c
+++ src/main/main.c
@@ -1484,8 +1484,8 @@ m64p_error main_run(void)
     if (count_per_op <= 0)
         count_per_op = ROM_SETTINGS.countperop;
 
-    if (count_per_op_denom_pot > 8)
-        count_per_op_denom_pot = 8;
+    if (count_per_op_denom_pot > 11)
+        count_per_op_denom_pot = 11;
 
     si_dma_duration = ConfigGetParamInt(g_CoreConfig, "SiDmaDuration");
     if (si_dma_duration < 0)
