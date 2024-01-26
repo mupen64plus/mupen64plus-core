49871d4e34f0ee726598dd61e68c1e443a87c10c
GhostlyDark <31742919+GhostlyDark@users.noreply.github.com>
2024-01-26 16:57:24 +0100

    Fixed read/write to RSP PC register from CPU
    Co-Authored-By: Matt Pharoah <mtpharoah@gmail.com>

--- src/device/rcp/rsp/rsp_core.c
+++ src/device/rcp/rsp/rsp_core.c
@@ -295,12 +295,20 @@ void read_rsp_regs2(void* opaque, uint32_t address, uint32_t* value)
     uint32_t reg = rsp_reg2(address);
 
     *value = sp->regs2[reg];
+
+    if (reg == SP_PC_REG)
+        *value &= 0xffc;
+
 }
 
 void write_rsp_regs2(void* opaque, uint32_t address, uint32_t value, uint32_t mask)
 {
     struct rsp_core* sp = (struct rsp_core*)opaque;
     uint32_t reg = rsp_reg2(address);
+
+    if (reg == SP_PC_REG)
+        mask &= 0xffc;
+
 
     masked_write(&sp->regs2[reg], value, mask);
 }
